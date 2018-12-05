e5ac02f937b9947fafd65c8554cfeed774419db7
Gene1994 <geneq@hotmail.com>
2018-12-05 23:05:36 +0800

    Initial commit

---
+++ demo-share-common/src/main/java/com/genequ/demosharecommon/DemoShareCommonApplication.java
@@ -0,0 +1,12 @@
+package com.genequ.demosharecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoShareCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoShareCommonApplication.class, args);
+	}
+}
